7536ee955ec5ecf0f21a9474386930cfed11eb40
furenku <furenku@gmail.com>
2017-02-11 12:58:55 -0600

    chatsnap v0.1

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # chatsnap
-chat didactico construido en nodejs 
+
+### chat didactico construido en nodejs
+
+
+## Instalación
+
+Necesitamos tener instalado node!
+
+```
+cd nombre/de/directorio/chatsnap
+npm i
+bower i
+node index.js
+```
+
+Abrir navegador y apuntar a localhost:3000
---
+++ index.js
@@ -0,0 +1,139 @@
+var express = require('express')
+var app = express()
+
+var http = require('http').Server(app)
+
+var io = require('socket.io')(http)
+
+// un Objeto para almacenar los usuarios:
+var users = {}
+
+// configurar algunos directorios para facilitar acceso en toda la app
+app.use('/bower_components', express.static('bower_components') )
+// app.use('/socket.io', express.static('socket.io') )
+app.use('/assets', express.static('public/assets') )
+
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + '/views/index.html' )
+});
+
+app.get('/user', function (req, res) {
+   res.send('Ver usuario');
+});
+
+
+
+
+io.on('connection', function(socket){
+
+   console.log('a user connected', socket.id )
+
+   users[ socket.id ] = {
+      socket: socket,
+      username: "Sin Nombre"
+   }
+
+   // console.log(users.length, users);
+   socket.on('disconnect', function(){
+
+      console.log('user disconnected', socket.id );
+
+
+      displayMessage = users[ socket.id ].username + " salió."
+
+
+      delete users[socket.id]
+
+      var userData = getUsers()
+
+      io.emit('user gone', {
+         message: displayMessage,
+         data: userData
+      });
+
+      // notificar numéro de usuarios restantes
+      // contando los índices en el Obj. de 'users'
+
+      console.log(
+         "todavía quedan "
+         + Object.keys(users).length
+         + " usuarios"
+      )
+
+   });
+
+
+   socket.on('chat message', function(msg){
+      console.log('message: ', socket.id, msg);
+
+      io.emit('chat message', {
+         username: users[socket.id].username,
+         message: msg
+      });
+
+   });
+
+
+
+   socket.on('private message', function(data){
+
+      sourceUser = socket.id
+      targetUser = users[data.target_id]
+      // .emit('hello')
+      message    = data.message
+
+
+      targetUser.socket.emit('private message', {
+         source_id: sourceUser,
+         username: users[sourceUser].username,
+         message: message
+      })
+
+
+   });
+
+   socket.on('set username', function(msg){
+
+      console.log('set username: ' + msg);
+
+      users[ socket.id ].username = msg;
+
+      var userData = getUsers()
+
+      displayMessage = users[ socket.id ].username + " acaba de entrar."
+
+      io.emit('new user', {
+         message: displayMessage,
+         data: userData
+      });
+
+   });
+
+
+
+});
+
+
+function getUsers() {
+
+   usersArray = []
+
+   // Metemos los datos relevantes de cada usuario:
+   for( indice in users ) {
+
+      // Introducimos cada usuario al arreglo
+      usersArray.push({
+         id: indice,
+         username: users[indice].username
+      })
+
+   }
+
+   return usersArray
+
+}
+
+http.listen(3000, function () {
+   console.log('Example app listening on port 3000!');
+});
